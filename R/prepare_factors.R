7567faa93de0b9d52a44c8283461fc8c1eaea462
Tazinho <malte.grosser@gmail.com>
2018-10-13 00:23:51 +0200

    this should resolve travis

--- R/prepare_factors.R
+++ R/prepare_factors.R
@@ -13,26 +13,6 @@
 #' @param return_mapping Logical. Should a mapping between integers and original characters be returned.
 #' 
 #' @return A contatenated vector of `train` and `test` transformed and converted regarding the \code{output_type}.
-#' 
-#' @examples
-#' ### data
-#' train <- dplyr::tibble(
-#' stay_same  = c('a','a','a','a','a','b','b','a','b','b','b', NA, NA),
-#' some_rare  = c('c','d','e','e','a','b','b','a','b','b','b', NA, NA),
-#' some_new   = c('c','d','e','e','a','b','b','a','b','b','b', NA, NA),
-#' integer    = c( 1L, 2L, 3L, 4L, 6L, 7L, 8L, 9L,10L,11L,12L,13L,14L),
-#' logical    = c(  TRUE,  FALSE,  TRUE,  FALSE,  TRUE,  FALSE,  TRUE,
-#'   FALSE,  TRUE,  FALSE,  TRUE,  FALSE,  TRUE))
-#' test  <- dplyr::tibble(
-#' stay_same  = c('a','a','a','a','a','b','b','a','b','b','b', NA, NA),
-#' some_rare  = c('c','c','c','c','c','d','b','a','b','b','b', NA, NA),
-#' some_new   = c('f','a','a','g','a','b','b','a','b','b','b', NA, NA),
-#' integer    = c( 1L, 2L, 3L, 4L, 6L, 7L, 8L, 9L,10L,11L,12L,13L,14L),
-#' logical    = c(  TRUE,  FALSE,  TRUE,  FALSE,  TRUE,  FALSE,  TRUE,  
-#' FALSE,  TRUE,  FALSE,  TRUE,  FALSE,  TRUE))
-#' ### preparation
-#' prepare_factors(train$some_new, test$some_new, output_type = "factor") 
-#' 
 #'
 #' @export
 #' 
@@ -61,6 +41,8 @@ prepare_factors <- function(train, test = NULL,
   if (rare_level %in% train_u)  warning("`rare_level` is already a level in `train`")
   if (rare_level %in% test_u)  warning("`rare_level` is already a level in `test`")
   # Count levels in the training data and change levels accordingly (while keeping NAs)
+  
+  
   df_train <- dplyr::tibble(train = train) %>% 
     dplyr::count(train) %>%
     dplyr::rename(count_train = n)
@@ -88,3 +70,5 @@ prepare_factors <- function(train, test = NULL,
   # return
   output
 }
+
+utils::globalVariables(c("n", "count_train"))
