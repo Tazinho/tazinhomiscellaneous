e21c9b61636bdf852d2d6a0ff26ab29f9318317c
Tazinho <malte.grosser@gmail.com>
2018-10-12 22:49:09 +0200

    try resolving travis

--- tests/testthat/test-prepare_factors.R
+++ tests/testthat/test-prepare_factors.R
@@ -6,12 +6,12 @@ test_that("initial poc",{
                          some_rare  = c('c','d','e','e','a','b','b','a','b','b','b', NA, NA),
                          some_new   = c('c','d','e','e','a','b','b','a','b','b','b', NA, NA),
                          integer    = c( 1L, 2L, 3L, 4L, 6L, 7L, 8L, 9L,10L,11L,12L,13L,14L),
-                         logical    = c(  T,  F,  T,  F,  T,  F,  T,  F,  T,  F,  T,  F,  T))
+                         logical    = c(  TRUE,  FALSE,  TRUE,  FALSE,  TRUE,  FALSE,  TRUE,  FALSE,  TRUE,  FALSE,  TRUE,  FALSE,  TRUE))
   test  <- dplyr::tibble(stay_same  = c('a','a','a','a','a','b','b','a','b','b','b', NA, NA),
                          some_rare  = c('c','c','c','c','c','d','b','a','b','b','b', NA, NA),
                          some_new   = c('f','a','a','g','a','b','b','a','b','b','b', NA, NA),
                          integer    = c( 1L, 2L, 3L, 4L, 6L, 7L, 8L, 9L,10L,11L,12L,13L,14L),
-                         logical    = c(  T,  F,  T,  F,  T,  F,  T,  F,  T,  F,  T,  F,  T))
+                         logical    = c(  TRUE,  FALSE,  TRUE,  FALSE,  TRUE,  FALSE,  TRUE,  FALSE,  TRUE,  FALSE,  TRUE,  FALSE,  TRUE))
   
   expect_equal(prepare_factors(train$some_new, test$some_new),
                c("rare", "rare", "rare", "rare", "rare", "b", "b", "rare", "b", 
