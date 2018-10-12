8be4d3d5011c6730a13b9a53426b436a9cf95880
Tazinho <malte.grosser@gmail.com>
2018-10-12 22:29:23 +0200

    resolve covr

--- tests/testthat/test-prepare_factors.R
+++ tests/testthat/test-prepare_factors.R
@@ -1,6 +1,6 @@
 context("prepare_factors")
 
-test_that("examples",{
+test_that("initial poc",{
   
   train <- dplyr::tibble(stay_same  = c('a','a','a','a','a','b','b','a','b','b','b', NA, NA),
                          some_rare  = c('c','d','e','e','a','b','b','a','b','b','b', NA, NA),
@@ -18,4 +18,39 @@ test_that("examples",{
                  "b", "b", NA, NA, "other", "rare", "rare", "other", "rare", "b", 
                  "b", "rare", "b", "b", "b", NA, NA)
   ) 
+})
+
+test_that("check that order of integers doesn't change",{
+  train <- c(rep("a", 5), rep("b", 2))
+  test  <- c(rep("a", 5))
+  
+  expect_equal(prepare_factors(train, test, output_type = "character"),
+               c("a", "a", "a", "a", "a", "rare", "rare", "a", "a", "a", "a", "a")
+               )
+  expect_equal(prepare_factors(train, test, output_type = "factor"),
+               structure(c(1L, 1L, 1L, 1L, 1L, 2L, 2L, 1L, 1L, 1L, 1L, 1L), 
+                         .Label = c("a", "rare", "other"), class = c("ordered", "factor"))
+               )
+  expect_equal(prepare_factors(train, test, output_type = "integer"),
+               c(1L, 1L, 1L, 1L, 1L, 2L, 2L, 1L, 1L, 1L, 1L, 1L)
+               )
+  
+  
+  train <- c(rep("a", 5), rep("b", 2))
+  test  <- c(rep("c", 3), rep("a", 5), rep("b", 5))
+  
+  expect_equal(prepare_factors(train, test, output_type = "character"),
+               c("a", "a", "a", "a", "a", "rare", "rare", "other", "other", 
+                 "other", "a", "a", "a", "a", "a", "rare", "rare", "rare", "rare", 
+                 "rare")
+               )
+  expect_equal(prepare_factors(train, test, output_type = "factor"),
+               structure(c(1L, 1L, 1L, 1L, 1L, 2L, 2L, 3L, 3L, 3L, 1L, 1L, 1L, 
+                           1L, 1L, 2L, 2L, 2L, 2L, 2L),
+                         .Label = c("a", "rare", "other"), class = c("ordered", "factor"))
+               )
+  expect_equal(prepare_factors(train, test, output_type = "integer"),
+               c(1L, 1L, 1L, 1L, 1L, 2L, 2L, 3L, 3L, 3L, 1L, 1L, 1L, 1L, 1L, 
+                 2L, 2L, 2L, 2L, 2L)
+               )
 })
