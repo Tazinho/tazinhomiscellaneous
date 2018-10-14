0626a4594862a907e45c5f6f1f0f1a22a4fbb6ba
Tazinho <malte.grosser@gmail.com>
2018-10-14 23:28:44 +0200

    Minimize test dependencies

--- tests/testthat/test-prepare_factors.R
+++ tests/testthat/test-prepare_factors.R
@@ -2,12 +2,12 @@ context("prepare_factors")
 
 test_that("initial poc",{
   
-  train <- dplyr::tibble(stay_same  = c('a','a','a','a','a','b','b','a','b','b','b', NA, NA),
+  train <- tibble::tibble(stay_same  = c('a','a','a','a','a','b','b','a','b','b','b', NA, NA),
                          some_rare  = c('c','d','e','e','a','b','b','a','b','b','b', NA, NA),
                          some_new   = c('c','d','e','e','a','b','b','a','b','b','b', NA, NA),
                          integer    = c( 1L, 2L, 3L, 4L, 6L, 7L, 8L, 9L,10L,11L,12L,13L,14L),
                          logical    = c(  TRUE,  FALSE,  TRUE,  FALSE,  TRUE,  FALSE,  TRUE,  FALSE,  TRUE,  FALSE,  TRUE,  FALSE,  TRUE))
-  test  <- dplyr::tibble(stay_same  = c('a','a','a','a','a','b','b','a','b','b','b', NA, NA),
+  test  <- tibble::tibble(stay_same  = c('a','a','a','a','a','b','b','a','b','b','b', NA, NA),
                          some_rare  = c('c','c','c','c','c','d','b','a','b','b','b', NA, NA),
                          some_new   = c('f','a','a','g','a','b','b','a','b','b','b', NA, NA),
                          integer    = c( 1L, 2L, 3L, 4L, 6L, 7L, 8L, 9L,10L,11L,12L,13L,14L),
