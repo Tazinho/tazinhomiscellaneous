c24d25a031724e2d6ed6aa343e8d2698bdd1ed1c
Tazinho <malte.grosser@gmail.com>
2018-10-16 14:22:33 +0200

    count via data.table

---
+++ R/prepare_factors_dt.R
@@ -0,0 +1,77 @@
+#' Preparing data for machine learning
+#' 
+#' Function to convert rare and new levels in test data
+#'
+#' @param train Character. Possibly a feature column from a training data set.
+#' @param test Character. Possibly a feature column from a test data set.
+#' @param rare_count Integer. Levels with less manifestations than \code{rare_count} will get the \code{rare_level}.
+#' @param rare_level Character. Specification of the new name for rare levels.
+#' @param new_level Character. The level for manifestations that didn't show up in the training data.
+#' @param ignore_na Logical. Should \code{NA}s \code{NA}?
+#' @param output_type Character ("character", "factor" or "integer"). If \code{"factor"} the output always gets the levels \code{rare_level} and \code{other_level}.
+#' @param encoding Character. Specifies the sorting of the levels / integers (when the \code{output_type} is not \code{"character"}.
+#' @param return_mapping Logical. Should a mapping between integers and original characters be returned.
+#' 
+#' @return A contatenated vector of `train` and `test` transformed and converted regarding the \code{output_type}.
+#' 
+#' ### preparation
+#' prepare_factors_dt(train$some_new, test$some_new, output_type = "factor") 
+#' 
+#' @import data.table
+#'
+#' @export
+#' 
+prepare_factors_dt <- function(train, test = NULL,
+                                 rare_count = 5L, rare_level = "rare",
+                                 new_level = "other", ignore_na = TRUE, 
+                                 output_type = c("character", "factor", "integer"),
+                                 encoding = NULL, return_mapping = FALSE) {
+  # Check arguments
+  if (!is.character(train)) stop("`train` must be a character.")
+  if (!(is.character(test) | is.null(test))) stop("`test` must be a character when it is provided.")
+  if (!is.integer(rare_count)) stop("`rare_count` must be an integer.")
+  if (!is.character(rare_level)) stop("`rare_level` must be a character.")
+  if (!is.character(new_level)) stop("`new_level` must be a character.")
+  if (!is.logical(ignore_na)) stop("`ignore_na` must be a logical.")
+  output_type <- match.arg(output_type)
+  if (!is.null(encoding)) stop("`encoding` is not implemented at the moment.")
+  if (!is.logical(return_mapping)) stop("`return_mapping` must be a logical.")
+  # Here should be a warning when `new_level` or `rare_level` already occur in the data
+  train_test <- c(train, test)
+  train_u <- unique(train)
+  test_u  <- unique(test)
+  train_test_u <- unique(train, test)
+  if (new_level %in% train_u)  warning("`new_level` is already a level in `train`.")
+  if (new_level %in% test_u)  warning("`new_level` is already a level in `test`.")
+  if (rare_level %in% train_u)  warning("`rare_level` is already a level in `train`.")
+  if (rare_level %in% test_u)  warning("`rare_level` is already a level in `test`.")
+  # Count levels in the training data and change levels accordingly (while keeping NAs)
+  df_train <- data.table::data.table(train = train)
+  df_train <- df_train[ , `:=`(count_train = .N), by = train] %>% unique()
+  df_train <- data.table::as.data.table(df_train)
+
+  df_train_test <- dplyr::tibble(train_test = train_test) %>% 
+    dplyr::left_join(df_train, by = c("train_test" = "train")) %>% 
+    dplyr::mutate(count_train = dplyr::if_else(is.na(count_train), 0L, count_train)) %>% 
+    dplyr::mutate(output = dplyr::case_when(
+      is.na(train_test)         ~ train_test,
+      count_train == 0L         ~ new_level,
+      count_train  < rare_count ~ rare_level,
+      TRUE                      ~ train_test
+    ))
+  # convert output
+  output <- df_train_test[["output"]]
+  if (output_type != "character") {
+    output_level <- df_train %>% dplyr::filter(count_train >= rare_count) %>%
+      dplyr::pull(train) %>% c(rare_level, new_level)
+    output <- factor(output, ordered = TRUE, levels = output_level)
+  }
+  
+  if (output_type == "integer") {
+    output <- as.integer(output) 
+  }
+  # - possibly add `mapping` attribute
+  # return
+  output
+}
+
---
+++ tests/testthat/test-prepare_factors_dt.R
@@ -0,0 +1,116 @@
+context("prepare_factors_dt")
+
+test_that("initial poc",{
+  
+  train <- data.frame(stay_same  = c('a','a','a','a','a','b','b','a','b','b','b', NA, NA),
+                      some_rare  = c('c','d','e','e','a','b','b','a','b','b','b', NA, NA),
+                      some_new   = c('c','d','e','e','a','b','b','a','b','b','b', NA, NA),
+                      integer    = c( 1L, 2L, 3L, 4L, 6L, 7L, 8L, 9L,10L,11L,12L,13L,14L),
+                      logical    = c(  TRUE,  FALSE,  TRUE,  FALSE,  TRUE,  FALSE,  TRUE,  FALSE,  TRUE,  FALSE,  TRUE,  FALSE,  TRUE), 
+                      stringsAsFactors = FALSE)
+  test  <- data.frame(stay_same  = c('a','a','a','a','a','b','b','a','b','b','b', NA, NA),
+                      some_rare  = c('c','c','c','c','c','d','b','a','b','b','b', NA, NA),
+                      some_new   = c('f','a','a','g','a','b','b','a','b','b','b', NA, NA),
+                      integer    = c( 1L, 2L, 3L, 4L, 6L, 7L, 8L, 9L,10L,11L,12L,13L,14L),
+                      logical    = c(  TRUE,  FALSE,  TRUE,  FALSE,  TRUE,  FALSE,  TRUE,  FALSE,  TRUE,  FALSE,  TRUE,  FALSE,  TRUE),
+                      stringsAsFactors = FALSE)
+  
+  expect_equal(prepare_factors_dt(train$some_new, test$some_new, rare_count = 5L),
+               c("rare", "rare", "rare", "rare", "rare", "b", "b", "rare", "b", 
+                 "b", "b", NA, NA, "other", "rare", "rare", "other", "rare", "b", 
+                 "b", "rare", "b", "b", "b", NA, NA)
+  ) 
+  
+  expect_equal(prepare_factors_dt(c(NA, NA, NA, NA, NA, "a"), 
+                                    c("b",NA, "a")),
+               c(NA, NA, NA, NA, NA, "rare", "other", NA, "rare"))
+})
+
+test_that("check that order of integers doesn't change",{
+  train <- c(rep("a", 5), rep("b", 2))
+  test  <- c(rep("a", 5))
+  
+  expect_equal(prepare_factors_dt(train, test, output_type = "character", rare_count = 5L),
+               c("a", "a", "a", "a", "a", "rare", "rare", "a", "a", "a", "a", "a")
+  )
+  expect_equal(prepare_factors_dt(train, test, output_type = "factor", rare_count = 5L),
+               structure(c(1L, 1L, 1L, 1L, 1L, 2L, 2L, 1L, 1L, 1L, 1L, 1L), 
+                         .Label = c("a", "rare", "other"), class = c("ordered", "factor"))
+  )
+  expect_equal(prepare_factors_dt(train, test, output_type = "integer", rare_count = 5L),
+               c(1L, 1L, 1L, 1L, 1L, 2L, 2L, 1L, 1L, 1L, 1L, 1L)
+  )
+  
+  
+  train <- c(rep("a", 5), rep("b", 2))
+  test  <- c(rep("c", 3), rep("a", 5), rep("b", 5))
+  
+  expect_equal(prepare_factors_dt(train, test, output_type = "character", rare_count = 5L),
+               c("a", "a", "a", "a", "a", "rare", "rare", "other", "other", 
+                 "other", "a", "a", "a", "a", "a", "rare", "rare", "rare", "rare", 
+                 "rare")
+  )
+  expect_equal(prepare_factors_dt(train, test, output_type = "factor", rare_count = 5L),
+               structure(c(1L, 1L, 1L, 1L, 1L, 2L, 2L, 3L, 3L, 3L, 1L, 1L, 1L, 
+                           1L, 1L, 2L, 2L, 2L, 2L, 2L),
+                         .Label = c("a", "rare", "other"), class = c("ordered", "factor"))
+  )
+  expect_equal(prepare_factors_dt(train, test, output_type = "integer", rare_count = 5L),
+               c(1L, 1L, 1L, 1L, 1L, 2L, 2L, 3L, 3L, 3L, 1L, 1L, 1L, 1L, 1L, 
+                 2L, 2L, 2L, 2L, 2L)
+  )
+  
+  
+  expect_equal(prepare_factors_dt(c("a", "a", "a")),
+               c("rare", "rare", "rare")
+  )
+  expect_equal(prepare_factors_dt(c("a", "a", "a", "a", "a")),
+               c("a", "a", "a", "a", "a")
+  )
+  expect_equal(prepare_factors_dt("a", NA_character_),
+               c("rare", NA)
+  )
+  
+})
+
+test_that("Input checks",{
+  
+  expect_error(prepare_factors_dt(TRUE),
+               "`train` must be a character.",
+               fixed = TRUE)
+  expect_error(prepare_factors_dt("a", TRUE),
+               '`test` must be a character when it is provided.',
+               fixed = TRUE)
+  expect_error(prepare_factors_dt("a", rare_count = TRUE),
+               '`rare_count` must be an integer.',
+               fixed = TRUE)
+  expect_error(prepare_factors_dt("a", rare_level = TRUE),
+               '`rare_level` must be a character.',
+               fixed = TRUE)
+  expect_error(prepare_factors_dt("a", new_level = TRUE),
+               '`new_level` must be a character.',
+               fixed = TRUE)
+  expect_error(prepare_factors_dt("a", ignore_na = "hat"),
+               '`ignore_na` must be a logical.',
+               fixed = TRUE)
+  expect_error(prepare_factors_dt("a", encoding = "hat"),
+               '`encoding` is not implemented at the moment.',
+               fixed = TRUE)
+  expect_error(prepare_factors_dt("a", return_mapping = "bla"),
+               '`return_mapping` must be a logical.',
+               fixed = TRUE)
+  expect_warning(prepare_factors_dt("other"),
+                 '`new_level` is already a level in `train`.',
+                 fixed = TRUE)
+  expect_warning(prepare_factors_dt("a", "other"),
+                 '`new_level` is already a level in `test`.',
+                 fixed = TRUE)
+  expect_warning(prepare_factors_dt("rare", "a"),
+                 '`rare_level` is already a level in `train`.',
+                 fixed = TRUE)
+  expect_warning(prepare_factors_dt("a", "rare"),
+                 '`rare_level` is already a level in `test`.',    
+                 fixed = TRUE)
+}
+)
+
